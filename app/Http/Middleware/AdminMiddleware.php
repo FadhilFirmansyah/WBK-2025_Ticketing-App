b3d731378bba1733eb92dad9b0fb58620a234baa
FadhilFirmansyah <fadhilfirmansyah100@gmail.com>
2026-01-17 01:53:34 +0000

    Feat: implement admin role middleware and update routing for admin dashboard

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->guard()->check() && auth()->guard()->user()->role === 'admin') {
+            return $next($request);
+        }
+
+        auth()->guard()->logout();
+        return redirect('/login');
+    }
+}
